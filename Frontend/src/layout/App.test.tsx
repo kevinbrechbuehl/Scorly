bb020da548f78ac8cd1a15ed8eb04f76a81ae3eb
Kevin Brechbühl <mail@aquasonic.ch>
2018-07-06 21:47:26 +0200

    Add jest-junit test reporter.

--- Frontend/src/layout/App.test.tsx
+++ Frontend/src/layout/App.test.tsx
@@ -2,8 +2,10 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('Scorly app', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
 });
