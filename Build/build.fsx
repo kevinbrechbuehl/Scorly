0be2238cee442e09da1f09c79573d2c242425c16
Kevin Brechbühl <mail@aquasonic.ch>
2018-07-20 16:28:08 +0200

    Add basic targets for build and test react frontend.

--- Build/build.fsx
+++ Build/build.fsx
@@ -1,27 +1,54 @@
 #r "paket:
+nuget Fake.IO.FileSystem
+nuget Fake.JavaScript.Npm
 nuget Fake.Core.Target //"
+#load "./.fake/build.fsx/intellisense.fsx"
 
 open Fake.Core
+open Fake.IO
+open Fake.JavaScript
 
-// *** Define Targets ***
+// Properties
+let outputDir = __SOURCE_DIRECTORY__ + "/Output"
+let tempDir = __SOURCE_DIRECTORY__ + "/Temp"
+
+let frontendDir = __SOURCE_DIRECTORY__ + "/../Frontend"
+let frontendBuildDir = frontendDir + "/build"
+let frontendTempDir = tempDir + "/Frontend"
+
+// Targets
 Target.create "Clean" (fun _ ->
-  Trace.log " --- Cleaning stuff --- "
+  Shell.deleteDirs [ outputDir; tempDir ]
 )
 
 Target.create "Build" (fun _ ->
-  Trace.log " --- Building the app --- "
+
+  // Build React Frontend
+  Npm.install (fun o -> { o with WorkingDirectory = frontendDir })
+  Npm.run "build" (fun o -> { o with WorkingDirectory = frontendDir })
+  Shell.copyDir frontendTempDir frontendBuildDir (fun _ -> true)
+)
+
+Target.create "Test" (fun _ ->
+
+  // Test React Frontend
+  Npm.run "test:ci" (fun o -> { o with WorkingDirectory = frontendDir })
 )
 
 Target.create "Deploy" (fun _ ->
-  Trace.log " --- Deploying app --- "
+  Shell.mkdir outputDir
+
+  // Pack React Frontend
+  // TODO: Create zip file of frontendTempDir and save into output folder
 )
 
 open Fake.Core.TargetOperators
 
-// *** Define Dependencies ***
+// Dependencies
 "Clean"
   ==> "Build"
+  ==> "Test"
   ==> "Deploy"
 
-// *** Start Build ***
+// Start Build
 Target.runOrDefault "Deploy"
